3c61eec1293400d18ea6e2e2e804ee290bab6368
benattimj <benatti1104@gmail.com>
2024-06-26 16:02:52 -0300

    Decrease and reset

--- index.js
+++ index.js
@@ -12,3 +12,13 @@ increaseBtn.onclick = function(){
     count++;
     countLabel.textContent = count;
 }
+
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent =count;
+}
